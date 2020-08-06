0733533efdd772341f002280041efbf48e730470
vancir <vancirprince@gmail.com>
2020-08-06 23:50:32 +0800

    GoSSIP-Bot Sync: 2020-08-06 23:50:32.091289

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 import os
 import re
+import sys
 import json
 import shutil
+import sqlite3
 import requests
+import logging
 from pathlib import Path
 from datetime import datetime
 from bs4 import BeautifulSoup as bs
 from markdownify import markdownify as md
 
-Token = "ddaf9e31-bad4-4cae-9714-bf81604fe4c2"
-RSSID = "6888bc4a-b31c-48c6-a42f-9ee3a5f4d0d4"
-WeRssApi = "https://wealert2.vux.li/api/v3/developers/subscribes/{RSSID}?token={Token}"
-WeRssUrl = WeRssApi.format(RSSID=RSSID, Token=Token)
+# initializing logger
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.StreamHandler(sys.stdout))
 
+config = json.load(open('config.json'))
+
+Archives = Path(config["Archives"])
+conn = sqlite3.connect(config["SQLite"])
+cursor = conn.cursor()
+WeRssApi = "https://wealert2.vux.li/api/v3/developers/subscribes/{RSSID}?token={Token}"
+WeRssUrl = WeRssApi.format(RSSID=config["RSSID"], Token=config["Token"])
+
+SHORT_LENGTH = 64
+LONG_LENGTH = 512
+DB_TABLE_NAME = "Articles"
+DB_TABLE_SCHEMA = "{name}( \
+    author varchar({short}), \
+    content_iid int, \
+    is_original boolean, \
+    idx int, \
+    mid varchar({short}), \
+    picture varchar({long}), \
+    posted_at varchar({short}) primary key, \
+    source_url varchar({long}), \
+    summary varchar({long}), \
+    title varchar({long}), \
+    type varchar({short}), \
+    url varchar({long}) \
+)".format(
+    name=DB_TABLE_NAME, 
+    short=SHORT_LENGTH, 
+    long=LONG_LENGTH
+)
+
+# remove invalid tags 
+# when converting html to markdown
 invalid_tags = ['span']
 
 def download(url, outdir):
@@ -27,17 +62,23 @@ def download(url, outdir):
         r.raw.decode_content = True
         shutil.copyfileobj(r.raw, f)
 
-def fixWxHtmlImg(html):
-    soup = bs(html, "html.parser")
+def fixWxHtmlImg(soup):
+    # add src attribute to img tag
     for img in soup.find_all("img"):
-        # add src attribute
         img["src"] = img["data-src"]
-
+    
+def removeInvalidTags(soup):
     # remove invalid tags but keep content
     for tag in invalid_tags:
         for match in soup.find_all(tag):
             match.replaceWithChildren()
 
+def fixWxHtml(soup):
+    fixWxHtmlImg(soup)
+    removeInvalidTags(soup)
+    return soup.prettify()
+
+def getPaperPdf(soup):
     paper_link = ''
     stub = soup.find(text=re.compile(".*PDF(:|：)"))
     if stub:
@@ -46,34 +87,74 @@ def fixWxHtmlImg(html):
         if paper.endswith('.pdf'):
             paper_link = paper
 
-    return soup.prettify(), paper_link
+    return paper_link
 
-def gitpush():
+def getWeRssDatas():
+    resp = requests.get(WeRssUrl)
+    obj = json.loads(resp.text)
+    lists = obj["data"]["list"]
+    return lists
+
+def pushToGithub():
     os.system("git add -A")
     os.system('git commit -m "GoSSIP-Bot Sync: {}"'.format(datetime.now()))
     os.system("git push origin master")
 
+def dbCreateTable():
+    cursor.execute("create table if not exists " + DB_TABLE_SCHEMA)
+
+def genDB():
+    pass
+
+def dbInsertRow(feed):
+    row = feed.copy()
+    del row["content"]
+
+    cols = ', '.join('"{}"'.format(col) for col in row.keys())
+    vals = ', '.join(':{}'.format(col) for col in row.keys())
+    sql = 'insert into "{0}" ({1}) values ({2})'.format(DB_TABLE_NAME, cols, vals)
+    cursor.execute(sql)
+
+def dbCheckArticleExists(feed):
+    sql = "select exists(select 1 from {table} where posted_at='{time}')".format(
+        table=DB_TABLE_NAME, time=feed['posted_at'])     
+
+    print(sql)   
+    cursor.execute(sql)
+    return True if cursor.fetchone() else False
+
+def parseFeed(feed):
+    # TODO: SQLite support and check dup
+    if dbCheckArticleExists(feed): return
+
+    outdir = Path("archives") / feed["title"]
+    if outdir.exists(): return
+    else: outdir.mkdir()
+
+    soup = bs(feed["content"], "html.parser")
+
+    html = fixWxHtml(soup)
+    # TODO: fix link contains '\_'
+    content = md(html)
+    output = outdir / 'README.md'
+    with output.open('w') as fp:
+        fp.write(content)
+
+    # paper = getPaperPdf(soup)
+    # if paper: download(paper, outdir)
+
+def clean():
+    conn.commit()
+    cursor.close()
+
 def main():
-    response = requests.get(WeRssUrl)
-    respObj = json.loads(response.text)
-    datalist = respObj["data"]["list"]
-
-    for metadata in datalist:
-        title = metadata["title"]
-
-        html, paper = fixWxHtmlImg(metadata["content"])
-        # TODO: fix link contains '\_'
-        content = md(html)
-        outdir = Path("archives") / title
-        if outdir.exists(): continue
-
-        outdir.mkdir(parents=True)
-        # if paper: download(paper, outdir)
-        output = outdir / 'README.md'
-        with output.open('w') as fp:
-            fp.write(content)
-
-    gitpush()
+    dbCreateTable()
+    feeds = getWeRssDatas()
+    for feed in feeds:
+        parseFeed(feed)
+    pushToGithub()
+    dbCreateTable()
+    clean()
 
 if __name__ == '__main__':
     main()
